e7c168a8ff99203c919fba6d3a965aca5a175e34
Maeve <mariya.kiryakulova@gmail.com>
2023-04-21 21:24:27 +0100

    add-jumbotron-to-app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './components/NavBar';
 import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from './components/Jumbotron';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Jumbotron/>
     </div>
   );
 }
