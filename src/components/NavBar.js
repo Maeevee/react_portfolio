f2af3b062e01955b570428be6b3d66b4ddca50cd
Maeve <mariya.kiryakulova@gmail.com>
2023-04-21 21:18:54 +0100

    customized-navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,25 +7,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavBar = () => {
 
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className={scrolled ? 'scralled' : ''} expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Brand href="#home">Maria K.</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#home" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home')}}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Projects</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
