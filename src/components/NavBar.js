e36d2ad3a631bfbd44a5dd40b744849c59af71c7
Maeve <mariya.kiryakulova@gmail.com>
2023-04-21 21:19:45 +0100

    add-fortawesome-icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSquarePhone } from '@fortawesome/free-solid-svg-icons'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const NavBar = () => {
 
@@ -19,6 +23,17 @@ const NavBar = () => {
                         <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Skills</Nav.Link>
                         <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Projects</Nav.Link>
                     </Nav>
+                    <span className="navbar-text">
+                        <div className='social-icon'>
+                            <a href="https://github.com/Maeevee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquareGithub} size="2x" style={{color: "#7d689c",}}/></a>
+                            <a href="https://www.linkedin.com/in/mariia-kiriakulova-2353a7241" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" style={{color: "#7d689c",}}/></a>
+                            <a href="mailto: mariya.kiryakulova@gmail.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquareEnvelope} size="2x" style={{color: "#7d689c",}}/></a>
+                            <a href="tel: +4407787387904" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquarePhone} size="2x" style={{color: "#7d689c",}}/></a>
+                        </div>
+                        <button className='contact' onClick={() => console.log('connect')}>
+                            <span>Contact me</span>
+                        </button>
+                    </span>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
