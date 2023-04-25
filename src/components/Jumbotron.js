b0e446448c1719ee2d1b541163ccdf69a89a046f
Maeve <mariya.kiryakulova@gmail.com>
2023-04-25 19:04:01 +0100

    add-hero-section

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import headerImg from "../assets/MaskgroupPhoto.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import CV from '../assets/CV_Mariia_Kiriakulova.pdf';
+
+const Jumbotron = () => {
+
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const greating = "Hello, I am Maria";
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+        tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+        }, [text]
+    );
+
+    const tick = () => {
+        let fullText = greating;
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setDelta(500);
+        }
+    }
+
+    const downloadCV = async() => {
+        const link = document.createElement('a');
+        link.href = CV;
+        link.setAttribute('download', 'CV_Mariia_Kiriakulova.pdf');
+        document.body.appendChild(link);
+        link.click();
+    }
+
+    return (
+        <section className="jumbotron" id="home">
+        <Container>
+            <Row className="aligh-items-center">
+            <Col xs={12} md={6} xl={7}>
+                <TrackVisibility>
+                {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                    <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Hello, I am Maria" ]'><span className="wrap">{text}</span></span></h1>
+                    <p>I'm an enthusiastic, motivated and detail-oriented Front-end Developer. I have a certain technical base and understanding of the main processes since I’ve studied biophysics and biochemistry at the university, I also consolidated all this knowledge and delved into Web Development at the Front-end Bootcamp.</p>
+                    <p>However, what sets me apart is my ability to approach web development from a scientific and analytical standpoint. I am able to break down complex problems into smaller components and devise solutions based on evidence and data.</p>  
+                    <p>Fast learner, hard worker and team player, passionate about building first-class websites.</p>
+                    <button className='contact' onClick={downloadCV}>
+                            <span>Download CV</span>
+                        </button>
+                </div>}
+                </TrackVisibility>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+                <TrackVisibility>
+                {({ isVisible }) =>
+                    <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                    <img src={headerImg} alt="Header Img"/>
+                    </div>}
+                </TrackVisibility>
+            </Col>
+            </Row>
+        </Container>
+        </section>
+    )
+}
+
+export default Jumbotron
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,9 +37,9 @@ const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home')}}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Projects</Nav.Link>
+                        <Nav.Link href="#home" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home')}}>Skills</Nav.Link>
+                        <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Projects</Nav.Link>
+                        <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Contacts</Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className='social-icon'>
@@ -48,9 +48,9 @@ const NavBar = () => {
                             <a href="mailto: mariya.kiryakulova@gmail.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquareEnvelope} size="2x" style={{color: "#7d689c",}}/></a>
                             <a href="tel: +4407787387904" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquarePhone} size="2x" style={{color: "#7d689c",}}/></a>
                         </div>
-                        <button className='contact' onClick={() => console.log('connect')}>
+                        {/* <button className='contact' onClick={() => console.log('connect')}>
                             <span>Contact me</span>
-                        </button>
+                        </button> */}
                     </span>
                 </Navbar.Collapse>
             </Container>
