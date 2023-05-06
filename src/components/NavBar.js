d2ec7bb26f566bde2fb14abf343d395977e24c53
Maeve <mariya.kiryakulova@gmail.com>
2023-05-06 09:52:02 +0100

    fix-navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -14,22 +14,24 @@ const NavBar = () => {
 
     useEffect(() => {
         const onScroll = () => {
-            if (Window.scrollY > 50) {
+            if (window.scrollY > 30) {
                 setScrolled(true);
             } else {
                 setScrolled(false);
             }
-            window.addEventListener('scroll', onScroll);
-            return () => window.removeEventListener('scroll', onScroll);
-        }
-    }, [])
+        };
+    
+        window.addEventListener('scroll', onScroll);
+    
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
 
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
     }
 
     return (
-        <Navbar className={scrolled ? 'scralled' : ''} expand="lg">
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="#home">Maria K.</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -37,9 +39,9 @@ const NavBar = () => {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home')}}>Skills</Nav.Link>
-                        <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Projects</Nav.Link>
-                        <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Contacts</Nav.Link>
+                        <Nav.Link href="#skills" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('home')}}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Projects</Nav.Link>
+                        <Nav.Link href="#contacts" className={ActiveLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Contacts</Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className='social-icon'>
@@ -48,9 +50,6 @@ const NavBar = () => {
                             <a href="mailto: mariya.kiryakulova@gmail.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquareEnvelope} size="2x" style={{color: "#7d689c",}}/></a>
                             <a href="tel: +4407787387904" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSquarePhone} size="2x" style={{color: "#7d689c",}}/></a>
                         </div>
-                        {/* <button className='contact' onClick={() => console.log('connect')}>
-                            <span>Contact me</span>
-                        </button> */}
                     </span>
                 </Navbar.Collapse>
             </Container>
