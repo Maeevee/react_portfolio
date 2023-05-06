977ba4e8642a8d55e4c4e5b2f13ab1ced8fd9254
Maeve <mariya.kiryakulova@gmail.com>
2023-05-07 00:38:03 +0100

    add-contacts-to-app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 import Jumbotron from './components/Jumbotron';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Jumbotron/>
       <Skills/>
       <Projects/>
+      <Footer/>
     </div>
   );
 }
