65f415eba8a0e8e83a21c6eb2ce60854354479a1
Maeve <mariya.kiryakulova@gmail.com>
2023-05-07 00:36:22 +0100

    add-projects-component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Container, Row, Col, } from "react-bootstrap";
-import projImg1 from "../assets/1140-organic-foods-quiz.jpg";
-// import projImg2 from "../assets/img/project-img2.png";
-// import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/Personal-Portfolio.jpg";
+import projImg2 from "../assets/Personal-Website.jpg";
+// import projImg3 from "../assets/Finance-Dashboard.jpg";
+import projImg4 from "../assets/Password-Generator.jpg";
+import projImg5 from "../assets/Code-Quiz.jpg";
+// import projImg6 from "../assets/Work-Day-Scheduler.jpg";
+import projImg7 from "../assets/Munchy.jpg";
+import projImg8 from "../assets/README-Generator.jpg";
+import projImg9 from "../assets/Team-Generator.jpg";
+import projImg10 from "../assets/ReadStream.jpg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -18,56 +25,63 @@ const Projects = () => {
         {
             title: "Personal Website",
             description: "Bootstrap",
-            imgUrl: projImg1,
+            imgUrl: projImg2,
             demo: "https://maeevee.github.io/Bootstrap-Portfolio-Kiriakulova/",
             code: "https://github.com/Maeevee/Bootstrap-Portfolio-Kiriakulova.git",
         },
+        // {
+        //     title: "Finance Dashboard",
+        //     description: "HTML,CSS & Javascript",
+        //     imgUrl: projImg1,
+        //     demo: "https://maeevee.github.io/Console-Finances/",
+        //     code: "https://github.com/Maeevee/Console-Finances.git",
+        //     },
         {
             title: "Password Generator",
             description: "Vanilla JavaScript",
-            imgUrl: projImg1,
+            imgUrl: projImg4,
             demo: "https://maeevee.github.io/Password-Generator/",
             code: "https://github.com/Maeevee/Password-Generator.git",
         },
         {
             title: "Code Quiz",
             description: "JavaScript & Wed APIs",
-            imgUrl: projImg1,
+            imgUrl: projImg5,
             demo: "https://maeevee.github.io/Code-Quiz/",
             code: "https://github.com/Maeevee/Code-Quiz.git",
         },
+        // {
+        // title: "Work Day Scheduler",
+        // description: "jQuery & Moment.js",
+        // imgUrl: projImg6,
+        // demo: "https://maeevee.github.io/Work-Day-Scheduler-by-Kiriakulova/",
+        // code: "https://github.com/Maeevee/Work-Day-Scheduler-by-Kiriakulova.git",
+        // },
         {
-        title: "Work Day Scheduler",
-        description: "jQuery & Moment.js",
-        imgUrl: projImg1,
-        demo: "https://maeevee.github.io/Work-Day-Scheduler-by-Kiriakulova/",
-        code: "https://github.com/Maeevee/Work-Day-Scheduler-by-Kiriakulova.git",
-        },
-        {
-            title: '"Munchy"',
+            title: "Munchy",
             description: "HTML, CSS, Bootstrap, jQuery & Server APIs",
-            imgUrl: projImg1,
+            imgUrl: projImg7,
             demo: "https://maeevee.github.io/Munchy/",
             code: "https://github.com/Maeevee/Munchy",
         },
         {
             title: "README Generator",
             description: "ES6 & Node.js",
-            imgUrl: projImg1,
+            imgUrl: projImg8,
             demo: "https://drive.google.com/file/d/1oggQQyKaFWSVfGlZJve232w4mqitewDK/view",
             code: "https://github.com/Maeevee/Professional-README-Generator-by-Kiriakulova.git",
         },
         {
             title: "Team Profile Generator",
             description: "JS, Node.js & Tests",
-            imgUrl: projImg1,
+            imgUrl: projImg9,
             demo: "https://github.com/Maeevee/Team-Profile-Generator-by-Kiriakulova/blob/main/output/team.html",
             code: "https://github.com/Maeevee/Team-Profile-Generator-by-Kiriakulova.git",
         },
         {
-            title: "Readstream",
+            title: "ReadStream",
             description: "React.js",
-            imgUrl: projImg1,
+            imgUrl: projImg10,
             demo: "https://readstream.netlify.app/",
             code: "https://github.com/mateusuk/ReadStream.git",
         },
@@ -84,7 +98,7 @@ const Projects = () => {
         <section className="project" id="projects">
         <Container>
             <Row>
-            <Col size={12}>
+            <Col size={12} className='col'>
                 <TrackVisibility>
                 {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
@@ -101,8 +115,8 @@ const Projects = () => {
                                                 <h4>{project.title}</h4>
                                                 <span>{project.description}</span>
                                                 <div className="cardButons">
-                                                    <button className='button-29'>Demo</button> 
-                                                    <button className='button-29'>Code</button>
+                                                    <a href={project.demo} target='_blank' rel="noreferrer"><button className='button-29'>Demo</button></a> 
+                                                    <a href={project.code} target='_blank' rel="noreferrer"><button className='button-29'>Code</button></a>
                                                 </div>
                                             </div>
                                         </div>
