441aa84833cb4b2774fd5f4e536669dabb90c711
Maeve <mariya.kiryakulova@gmail.com>
2023-05-08 17:56:00 +0100

    Revert "update-contacts"
    This reverts commit c7201f211d48eaaadaeca4e0fed2660b5c2458ac.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/Rectangle.png";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
 const Contacts = () => {
 
     const formInitialDetails = {
-        Name: '',
+        firstName: '',
+        lastName: '',
         email: '',
         phone: '',
         message: ''
@@ -33,91 +32,56 @@ const Contacts = () => {
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
-        body: JSON.stringify({
-            firstName: formDetails.Name,
-            email: formDetails.email,
-            phone: formDetails.phone,
-            message: formDetails.message
-        }),
+        body: JSON.stringify(formDetails),
         });
         setButtonText("Send");
         let result = await response.json();
         setFormDetails(formInitialDetails);
         if (result.code === 200) {
-            toast.success('Message sent successfully', {
-                position: "bottom-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-                });
+        setStatus({ succes: true, message: 'Message sent successfully'});
         } else {
-            toast.error('Something went wrong, please try again later.', {
-                position: "bottom-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-                });
+        setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
         }
     };
 
-
     return (
-        <section className="contact" id="contact">
+        <section className="contact" id="contacts">
             <Container>
                 <Row className="align-items-center">
                     <Col size={12} md={6}>
-                        <TrackVisibility offset={50}>
+                        <TrackVisibility>
                         {({ isVisible }) =>
                             <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
                         }
                         </TrackVisibility>
                     </Col>
                     <Col size={12} md={6}>
-                        <TrackVisibility >
+                        <TrackVisibility>
                         {({ isVisible }) =>
                             <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                             <h2>Contact me</h2>
                             <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col size={12} sm={12} className="px-1">
-                                <input type="text" value={formDetails.Name} placeholder="Name" onChange={(e) => onFormUpdate('Name', e.target.value)} required/>
+                                <input type="text" value={formDetails.firstName} placeholder="Name" onChange={(e) => onFormUpdate('Name', e.target.value)} />
                                 </Col>
                                 <Col size={12} sm={12} className="px-1">
-                                <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} required/>
+                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                                 </Col>
                                 <Col size={12} sm={12} className="px-1">
-                                <input type="tel" value={formDetails.phone} placeholder="Phone" onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                                <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                                 </Col>
                                 <Col size={12} className="px-1">
-                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)} required></textarea>
-                                <button className="submitButton" type="submit"><span>{buttonText}</span></button>
+                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                                <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
-                            </Row>
-                            <Row>
-                                <Col className="ToastContainerParent">
-                                        <ToastContainer toastClassName="toast-container" bodyClassName="toast-body"
-                                            position="bottom-center"
-                                            autoClose={3000}
-                                            hideProgressBar={false}
-                                            newestOnTop={false}
-                                            closeOnClick={true}
-                                            rtl={false}
-                                            pauseOnFocusLoss={true}
-                                            draggable={true}
-                                            pauseOnHover={true}
-                                            theme="dark"
-                                        />
+                                {
+                                status.message &&
+                                <Col>
+                                    <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
                                 </Col>
+                                }
                             </Row>
-                            
                             </form>
                         </div>}
                         </TrackVisibility>
