065f5b22b3f21a5ce56a851f743e46bb31260e1f
Maeve <mariya.kiryakulova@gmail.com>
2023-05-14 22:40:06 +0100

    animate-navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,12 +2,25 @@ import { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 
 const NavBar = () => {
     const [ActiveLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
+
+const animateNav = {
+    hidden: {
+        opacity: 0,
+        y: -50,
+        transition: {type: 'spring', stiffnes: 1000, dumping: 740}
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {type: 'spring', stiffnes: 1000, delay: 0.3}
+}}
+
 
     useEffect(() => {
         const onScroll = () => {
@@ -30,20 +43,24 @@ const NavBar = () => {
     return (
         <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
             <Container>
-                <Navbar.Brand href="#home">Maria K.</Navbar.Brand>
+                <motion.div variants={animateNav} initial='hidden' whileInView='show'>
+                    <Navbar.Brand href="#home">Maria K.</Navbar.Brand>
+                </motion.div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#skills" className={ActiveLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills')}}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={ActiveLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects')}}>Projects</Nav.Link>
-                        <Nav.Link href="#contact" className={ActiveLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('contact')}}>Contacts</Nav.Link>
-                    </Nav>
-                    
+                    <motion.div className="ms-auto" variants={animateNav} initial='hidden' whileInView='show'>
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#skills" className={ActiveLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('skills'); document.querySelector('.navbar-collapse').classList.remove('show')}}>Skills</Nav.Link>
+                            <Nav.Link href="#projects" className={ActiveLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('projects'); document.querySelector('.navbar-collapse').classList.remove('show')}}>Projects</Nav.Link>
+                            <Nav.Link href="#contact" className={ActiveLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => {onUpdateActiveLink('contact'); document.querySelector('.navbar-collapse').classList.remove('show')}}>Contacts</Nav.Link>
+                        </Nav>
+                    </motion.div>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+        
     );
 }
 
